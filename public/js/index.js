45e267ca4be6bab9497b765b9cd96d63a884c7d9
ravi <rmanshahia@gmail.com>
2018-10-17 17:32:55 +0530

    Added CSS for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -31,11 +31,13 @@ socket.on('disconnect', () => {
 jQuery('#message-form').on('submit', function(e) {
   e.preventDefault();
 
+  var messageTextBox = jQuery('[name=message]');
+
   socket.emit('createMessage', {
     from: 'User',
-    text: jQuery('[name=message]').val()
+    text: messageTextBox.val()
   }, function() {
-
+    messageTextBox.val("");
   });
 });
 
@@ -45,13 +47,16 @@ locationButton.on('click', function() {
     return alert("Geolocation not available");
   }
 
+  locationButton.attr('disabled', 'disabled').text('Sending location ...');
   navigator.geolocation.getCurrentPosition( function( position )
   {
+    locationButton.removeAttr('disabled').text('Send Location');
       socket.emit('createLocationMessage', {
         latitude: position.coords.latitude,
         longitude: position.coords.longitude
       });
   }, function(error) {
     alert("Unable to fetch location");
+    locationButton.removeAttr('disabled').text("Send Location");
   });
 });
