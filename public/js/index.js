c2cc384423c78a0651006c8ba25ebb1c9824af67
ravi <rmanshahia@gmail.com>
2018-10-17 15:25:46 +0530

    Geolocation Part 2

--- public/js/index.js
+++ public/js/index.js
@@ -13,6 +13,17 @@ socket.on('newMessage', function(message) {
   jQuery('#messages').append(li);
 });
 
+socket.on('newLocationMessage', function(message) {
+  var li = jQuery('<li></li>');
+  var a = jQuery('<a target="_blank">My Current Location</a>');
+
+  li.text(`${message.from}: `);
+  a.attr('href',message.url);
+  li.append(a);
+  jQuery('#messages').append(li);
+
+});
+
 socket.on('disconnect', () => {
   console.log('Disconnected from server');
 });
