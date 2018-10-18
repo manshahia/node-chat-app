0c16f361f9c380deabb31a45e6db3788140f2dba
ravi <rmanshahia@gmail.com>
2018-10-18 15:35:55 +0530

    Added Users class

---
+++ server/utils/users.js
@@ -0,0 +1,52 @@
+[{
+  id: 'socket ID',
+  name: 'Andere',
+  room: 'The ffo dans'
+}]
+
+
+
+//addUser(id, name, room) method
+//removeUser(id,)
+//getUser(id)
+//getUserList(room)
+
+
+class Users {
+  constructor () {
+    this.users = [];
+  }
+
+  addUser (id, name, room) {
+    var user = { id, name, room };
+    this.users.push(user);
+    return user;
+  }
+
+  removeUser (id) {
+    //return user that was removed.
+    var userToRemove = this.getUser(id);
+    if(userToRemove) {
+      this.users = this.users.filter( (user) => user.id !== id);
+    }
+    return userToRemove;
+  }
+
+  getUser (id) {
+    return userToFind = this.users.find( user => user.id === id);
+  }
+
+  getUserList (room) {
+    var users = this.users.filter( (user) => {
+      return user.room === room;
+    });
+
+    var namesArray = users.map( (user) => {
+      return user.name;
+    });
+
+    return namesArray;
+  }
+}
+
+module.exports = {Users};
